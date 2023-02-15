774014eddc5970d820fdb30618a4d36f603fe55e
takumi_kurogi <takumines1@gmail.com>
2023-02-16 01:19:15 +0900

    [update] 検索画面コンポーネントのレイアウト修正

--- resources/ts/Pages/search/Index.tsx
+++ resources/ts/Pages/search/Index.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { SearchInput } from '~/components/SearchInput'
+import { SearchHeading } from '~/components/SearchHeading'
+import { Center, VStack } from '@chakra-ui/react'
+import { SearchButton } from '~/components/SearchButton'
 
-export default function Welcome() {
-  return <h1>Welcome Inertia.js with React</h1>
+export default function Index() {
+  return (
+    <>
+      <VStack marginTop={180} spacing={10}>
+        <Center>
+          <SearchHeading size={'large'} title={'Goggle'} />
+        </Center>
+        <Center>
+          <SearchInput />
+        </Center>
+        <Center>
+          <SearchButton title={'検索'} />
+        </Center>
+      </VStack>
+    </>
+  )
 }
